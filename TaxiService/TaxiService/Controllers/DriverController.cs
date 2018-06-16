51def26aa68a3fd5198d6a1d1c86389cf9197bb7
ivanamar996 <markovic.ivana996@gmail.com>
2018-06-16 22:29:38 +0200

    Dodat DriverController, omoguceno vozacima da mijenjaju ternutnu lokaciju

---
+++ TaxiService/TaxiService/Controllers/DriverController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaxiService.Models;
+
+namespace TaxiService.Controllers
+{
+    public class DriverController : ApiController
+    {
+        [HttpPost]
+        [Route("api/Driver/EditLocation")]
+        public void Edit([FromBody]Location location)
+        {
+            Driver driverEdit = Data.driverServices.RetriveDriverByUserName(Data.loggedUser.Username);
+            driverEdit.Location = location;
+            Data.driverServices.EditDriverProfile(driverEdit);
+        }
+    }
+}
