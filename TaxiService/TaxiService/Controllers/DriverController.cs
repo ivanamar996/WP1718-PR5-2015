dc403aa146421a3231b4b2b5b114c9dfb3ad9a3b
ivanamar996 <markovic.ivana996@gmail.com>
2018-06-18 20:14:42 +0200

    Omoguceno vozacima da promjene status voznje u Uspjesna

--- TaxiService/TaxiService/Controllers/DriverController.cs
+++ TaxiService/TaxiService/Controllers/DriverController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -17,6 +18,58 @@ namespace TaxiService.Controllers
             Driver driverEdit = Data.driverServices.RetriveDriverByUserName(Data.loggedUser.Username);
             driverEdit.Location = location;
             Data.driverServices.EditDriverProfile(driverEdit);
+        }
+
+        [HttpPost]
+        [Route("api/Driver/SuccessfulDrive")]
+        public void SuccessfulDrive([FromBody]JObject data)
+        {            
+            IEnumerable<Drive> drives = Data.driveServices.RetriveAllDrives();
+            Drive driveFinis = new Drive();
+
+            foreach(Drive d in drives)
+            {
+                if(d.DrivedBy == Data.driverServices.RetriveDriverById(Data.loggedUser.Id))
+                {
+                    driveFinis = d;
+                }
+            }
+
+            driveFinis.State = Enums.Status.Successful;
+            driveFinis.Destination = new Location
+            {
+                X = Double.Parse(data.GetValue("x").ToString()),
+                Y = Double.Parse(data.GetValue("y").ToString()),
+                Address = data.GetValue("address").ToString()
+            };
+            driveFinis.Price = Int32.Parse(data.GetValue("price").ToString());
+            Data.driveServices.EditDriveProfile(driveFinis);
+        }
+
+        [HttpPost]
+        [Route("api/Driver/UnsuccessfulDrive")]
+        public void UnsuccessfulDrive([FromBody]JObject data)
+        {
+            IEnumerable<Drive> drives = Data.driveServices.RetriveAllDrives();
+            Drive driveFinis = new Drive();
+
+            foreach (Drive d in drives)
+            {
+                if (d.DrivedBy == Data.driverServices.RetriveDriverById(Data.loggedUser.Id))
+                {
+                    driveFinis = d;
+                }
+            }
+
+            driveFinis.State = Enums.Status.Successful;
+            driveFinis.Destination = new Location
+            {
+                X = Double.Parse(data.GetValue("x").ToString()),
+                Y = Double.Parse(data.GetValue("y").ToString()),
+                Address = data.GetValue("address").ToString()
+            };
+            driveFinis.Price = Int32.Parse(data.GetValue("price").ToString());
+            Data.driveServices.EditDriveProfile(driveFinis);
         }
     }
 }
